1e3d064335c3af42cc354531a6e86dfbb30d99a6
KonradStaniec <konrad.staniec@gmail.com>
2023-07-28 07:37:01 +0200

    Change rpc client (#40)
    * Change rpc client

--- valcfg/babylon.go
+++ valcfg/babylon.go
@@ -1,8 +1,9 @@
 package valcfg
 
 import (
-	bbncfg "github.com/babylonchain/rpc-client/config"
 	"time"
+
+	"github.com/cosmos/relayer/v2/relayer/chains/cosmos"
 )
 
 type BBNConfig struct {
@@ -23,46 +24,41 @@ type BBNConfig struct {
 }
 
 func DefaultBBNConfig() BBNConfig {
-	dc := bbncfg.DefaultBabylonConfig()
 	//fill up the config from dc config
 	return BBNConfig{
-		Key:            dc.Key,
-		ChainID:        dc.ChainID,
-		RPCAddr:        dc.RPCAddr,
-		GRPCAddr:       dc.GRPCAddr,
-		AccountPrefix:  dc.AccountPrefix,
-		KeyringBackend: dc.KeyringBackend,
-		GasAdjustment:  dc.GasAdjustment,
-		GasPrices:      dc.GasPrices,
-		KeyDirectory:   DefaultValdDir,
-		Debug:          dc.Debug,
-		Timeout:        dc.Timeout,
+		Key:            "node0",
+		ChainID:        "chain-test",
+		RPCAddr:        "http://localhost:26657",
+		GRPCAddr:       "https://localhost:9090",
+		AccountPrefix:  "bbn",
+		KeyringBackend: "test",
+		GasAdjustment:  1.2,
+		GasPrices:      "0.01ubbn",
+		KeyDirectory:   defaultDataDir,
+		Debug:          true,
+		Timeout:        20 * time.Second,
 		// Setting this to relatively low value, out currnet babylon client (lens) will
 		// block for this amout of time to wait for transaction inclusion in block
 		BlockTimeout: 1 * time.Minute,
-		OutputFormat: dc.OutputFormat,
-		SignModeStr:  dc.SignModeStr,
+		OutputFormat: "json",
+		SignModeStr:  "direct",
 	}
 }
 
-func BBNConfigToBabylonConfig(bc *BBNConfig) bbncfg.BabylonConfig {
-	return bbncfg.BabylonConfig{
+func BBNConfigToCosmosProviderConfig(bc *BBNConfig) cosmos.CosmosProviderConfig {
+	return cosmos.CosmosProviderConfig{
 		Key:            bc.Key,
 		ChainID:        bc.ChainID,
 		RPCAddr:        bc.RPCAddr,
-		GRPCAddr:       bc.GRPCAddr,
 		AccountPrefix:  bc.AccountPrefix,
 		KeyringBackend: bc.KeyringBackend,
 		GasAdjustment:  bc.GasAdjustment,
 		GasPrices:      bc.GasPrices,
 		KeyDirectory:   bc.KeyDirectory,
 		Debug:          bc.Debug,
-		Timeout:        bc.Timeout,
-		BlockTimeout:   bc.BlockTimeout,
+		Timeout:        bc.Timeout.String(),
+		BlockTimeout:   bc.BlockTimeout.String(),
 		OutputFormat:   bc.OutputFormat,
 		SignModeStr:    bc.SignModeStr,
-		// it is weird that this is client config, as this is address which is put into
-		// checkpoint, and has nothing to do with rpc client
-		SubmitterAddress: "",
 	}
 }
