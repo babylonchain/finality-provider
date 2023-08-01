959963885cf560dc2673c44b43f85d894751fb62
Vitalis Salis <vitsalis@gmail.com>
2023-08-01 11:38:54 +0300

    Config updates

--- valcfg/validator.go
+++ valcfg/validator.go
@@ -1,9 +1,5 @@
 package valcfg
 
-var (
-	defaultAutoChainScanningMode = true
-)
-
 type ValidatorConfig struct {
 	StaticChainScanningStartHeight uint64 `long:"staticchainscanningstartheight" description:"The static height from which we start polling the chain"`
 	AutoChainScanningMode          bool   `long:"autochainscanningmode" description:"Automatically discover the height from which to start polling the chain"`
@@ -11,7 +7,7 @@ type ValidatorConfig struct {
 
 func DefaultValidatorConfig() ValidatorConfig {
 	return ValidatorConfig{
-		StaticChainScanningStartHeight: 0,
-		AutoChainScanningMode:          defaultAutoChainScanningMode,
+		StaticChainScanningStartHeight: 1,
+		AutoChainScanningMode:          true,
 	}
 }
