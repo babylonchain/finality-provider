d2aae7d1d73e8e9cfc9cc752f9d7e7d938a6c274
Vitalis Salis <vitsalis@gmail.com>
2023-08-01 11:29:23 +0300

    fix

--- valcfg/validator.go
+++ valcfg/validator.go
@@ -5,7 +5,7 @@ var (
 )
 
 type ValidatorConfig struct {
-	StaticChainScanningStartHeight uint64 `long:"staticchainscanningstartheight" desciption:"The static height from which we start polling the chain"`
+	StaticChainScanningStartHeight uint64 `long:"staticchainscanningstartheight" description:"The static height from which we start polling the chain"`
 	AutoChainScanningMode          bool   `long:"autochainscanningmode" description:"Automatically discover the height from which to start polling the chain"`
 }
 
