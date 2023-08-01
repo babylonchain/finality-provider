ba8be26f838a4707249fe2e77328e29f01a06ad2
Vitalis Salis <vitsalis@gmail.com>
2023-08-01 11:26:48 +0300

    fix: Remove pointer from StaticChainScanningStartHeight

--- valcfg/validator.go
+++ valcfg/validator.go
@@ -5,13 +5,13 @@ var (
 )
 
 type ValidatorConfig struct {
-	StaticChainScanningStartHeight *uint64 `long:"staticchainscanningstartheight" desciption:"The static height from which we start polling the chain"`
-	AutoChainScanningMode          bool    `long:"autochainscanningmode" description:"Automatically discover the height from which to start polling the chain"`
+	StaticChainScanningStartHeight uint64 `long:"staticchainscanningstartheight" desciption:"The static height from which we start polling the chain"`
+	AutoChainScanningMode          bool   `long:"autochainscanningmode" description:"Automatically discover the height from which to start polling the chain"`
 }
 
 func DefaultValidatorConfig() ValidatorConfig {
 	return ValidatorConfig{
-		StaticChainScanningStartHeight: nil,
+		StaticChainScanningStartHeight: 0,
 		AutoChainScanningMode:          defaultAutoChainScanningMode,
 	}
 }
