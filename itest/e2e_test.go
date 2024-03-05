dbbe3d4de279c9589fd4e93b718e4d92f31be6c9
Fangyu Gai <greferry@gmail.com>
2024-03-05 09:44:22 +0800

    fix e2e

--- itest/e2e_test.go
+++ itest/e2e_test.go
@@ -104,7 +104,8 @@ func TestDoubleSigning(t *testing.T) {
 	// try to start all the finality providers and the slashed one should not be restarted
 	err = tm.Fpa.StartHandlingAll()
 	require.NoError(t, err)
-	fps := tm.Fpa.ListAllFinalityProvidersInfo()
+	fps, err := tm.Fpa.ListAllFinalityProvidersInfo()
+	require.NoError(t, err)
 	require.Equal(t, 1, len(fps))
 	require.Equal(t, proto.FinalityProviderStatus_name[4], fps[0].Status)
 	require.Equal(t, false, fps[0].IsRunning)
