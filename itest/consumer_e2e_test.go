021b2447e916c6b3e401f71107c66dbc3c2f714f
Gurjot <gurjotsyd@gmail.com>
2024-06-04 19:46:33 -0400

    fix spaces

--- itest/consumer_e2e_test.go
+++ itest/consumer_e2e_test.go
@@ -13,14 +13,13 @@ import (
 func TestConsumerStoreContract(t *testing.T) {
 	tm, _, _ := StartManagerWithFinalityProvider(t, 1)
 	defer tm.Stop(t)
-
 	// Store the Babylon contract in the consumer chain
 	babylonContractPath := "wasmd_contracts/babylon_contract.wasm"
 	storedCodeID, _, err := tm.WasmdHandler.StoreWasmCode(babylonContractPath)
 	require.NoError(t, err)
-	// Query the latest code ID from wasmd q wasm list-code
+	// Query the latest code ID from "wasmd q wasm list-code"
 	latestCodeId, err := tm.WasmdHandler.GetLatestCodeID()
 	require.NoError(t, err)
-	// Assert that the code ID returned from store-code and list-code is the same
+	// Assert that the code id returned from store-code and list-code is the same
 	require.Equal(t, storedCodeID, latestCodeId)
 }
