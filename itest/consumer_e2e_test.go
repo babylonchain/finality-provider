f55b42001485b6dc0cd3ea6104b6609354fe506d
Gurjot <gurjotsyd@gmail.com>
2024-06-06 15:25:34 -0400

    test

--- itest/consumer_e2e_test.go
+++ itest/consumer_e2e_test.go
@@ -22,17 +22,17 @@ func TestConsumerFinalityProviderRegistration(t *testing.T) {
 }
 
 // TestConsumerStoreContract stores a contract in the consumer chain
-//func TestConsumerStoreContract(t *testing.T) {
-//	ctm, _ := StartConsumerManagerWithFps(t, 1)
-//	defer ctm.Stop(t)
-//
-//	// Store the Babylon contract in the consumer chain
-//	babylonContractPath := "bytecode/babylon_contract.wasm"
-//	storedCodeID, _, err := ctm.WasmdHandler.StoreWasmCode(babylonContractPath)
-//	require.NoError(t, err)
-//	// Query the latest code ID from "wasmd q wasm list-code"
-//	latestCodeID, err := ctm.WasmdHandler.GetLatestCodeID()
-//	require.NoError(t, err)
-//	// Assert that the code id returned from store-code and list-code is the same
-//	require.Equal(t, storedCodeID, latestCodeID)
-//}
+func TestConsumerStoreContract(t *testing.T) {
+	ctm, _ := StartConsumerManagerWithFps(t, 1)
+	defer ctm.Stop(t)
+
+	// Store the Babylon contract in the consumer chain
+	babylonContractPath := "bytecode/babylon_contract.wasm"
+	storedCodeID, _, err := ctm.WasmdHandler.StoreWasmCode(babylonContractPath)
+	require.NoError(t, err)
+	// Query the latest code ID from "wasmd q wasm list-code"
+	latestCodeID, err := ctm.WasmdHandler.GetLatestCodeID()
+	require.NoError(t, err)
+	// Assert that the code id returned from store-code and list-code is the same
+	require.Equal(t, storedCodeID, latestCodeID)
+}
