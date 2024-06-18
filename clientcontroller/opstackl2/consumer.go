b3fdf7611ae502d23a1e73577048dd4a08f80019
lesterli <fulfilself@gmail.com>
2024-06-18 15:15:06 -0400

    add opl2Client

--- clientcontroller/opstackl2/consumer.go
+++ clientcontroller/opstackl2/consumer.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
+	"math/big"
 
 	sdkErr "cosmossdk.io/errors"
 	wasmtypes "github.com/CosmWasm/wasmd/x/wasm/types"
@@ -18,15 +19,17 @@ import (
 	cmtcrypto "github.com/cometbft/cometbft/proto/tendermint/crypto"
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/cosmos/relayer/v2/relayer/provider"
+	"github.com/ethereum/go-ethereum/ethclient"
 	"go.uber.org/zap"
 )
 
 var _ api.ConsumerController = &OPStackL2ConsumerController{}
 
 type OPStackL2ConsumerController struct {
-	bbnClient *bbnclient.Client
-	cfg       *fpcfg.OPStackL2Config
-	logger    *zap.Logger
+	bbnClient  *bbnclient.Client
+	opl2Client *ethclient.Client
+	cfg        *fpcfg.OPStackL2Config
+	logger     *zap.Logger
 }
 
 func NewOPStackL2ConsumerController(
@@ -44,8 +47,18 @@ func NewOPStackL2ConsumerController(
 	if err != nil {
 		return nil, fmt.Errorf("failed to create Babylon client: %w", err)
 	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), opl2Cfg.Timeout)
+	defer cancel()
+
+	opl2Client, err := ethclient.DialContext(ctx, opl2Cfg.OPStackL2RPCAddress)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create OPStack L2 client: %w", err)
+	}
+
 	return &OPStackL2ConsumerController{
 		bbnClient,
+		opl2Client,
 		opl2Cfg,
 		logger,
 	}, nil
@@ -204,98 +217,63 @@ func (cc *OPStackL2ConsumerController) SubmitBatchFinalitySigs(
 }
 
 // QueryFinalityProviderVotingPower queries the voting power of the finality provider at a given height
-func (ec *OPStackL2ConsumerController) QueryFinalityProviderVotingPower(fpPk *btcec.PublicKey, blockHeight uint64) (uint64, error) {
-	/* TODO: implement
-
-	latest_committed_l2_height = read `latestBlockNumber()` from the L1 L2OutputOracle contract and return the result
-
-	if blockHeight > latest_committed_l2_height:
-
-		query the VP from the L1 oracle contract using "latest" as the block tag
-
-	else:
-
-		1. query the L1 event `emit OutputProposed(_outputRoot, nextOutputIndex(), _l2BlockNumber, block.timestamp, block.number);`
-		  to find the first event where the `_l2BlockNumber` >= blockHeight
-		2. get the block.number from the event
-		3. query the VP from the L1 oracle contract using `block.number` as the block tag
-
-	*/
-
+func (cc *OPStackL2ConsumerController) QueryFinalityProviderVotingPower(fpPk *btcec.PublicKey, blockHeight uint64) (uint64, error) {
 	return 0, nil
 }
 
-func (ec *OPStackL2ConsumerController) QueryLatestFinalizedBlock() (*types.BlockInfo, error) {
+func (cc *OPStackL2ConsumerController) QueryLatestFinalizedBlock() (*types.BlockInfo, error) {
 	return &types.BlockInfo{
 		Height: 0,
 		Hash:   nil,
 	}, nil
 }
 
-func (ec *OPStackL2ConsumerController) QueryBlocks(startHeight, endHeight, limit uint64) ([]*types.BlockInfo, error) {
-
-	return ec.queryLatestBlocks(sdk.Uint64ToBigEndian(startHeight), 0, finalitytypes.QueriedBlockStatus_ANY, false)
+func (cc *OPStackL2ConsumerController) QueryBlocks(startHeight, endHeight, limit uint64) ([]*types.BlockInfo, error) {
+	return cc.queryLatestBlocks(sdk.Uint64ToBigEndian(startHeight), 0, finalitytypes.QueriedBlockStatus_ANY, false)
 }
 
-func (ec *OPStackL2ConsumerController) queryLatestBlocks(startKey []byte, count uint64, status finalitytypes.QueriedBlockStatus, reverse bool) ([]*types.BlockInfo, error) {
+func (cc *OPStackL2ConsumerController) queryLatestBlocks(startKey []byte, count uint64, status finalitytypes.QueriedBlockStatus, reverse bool) ([]*types.BlockInfo, error) {
 	var blocks []*types.BlockInfo
-
 	return blocks, nil
 }
 
-func (ec *OPStackL2ConsumerController) QueryBlock(height uint64) (*types.BlockInfo, error) {
+// QueryBlock gets the L2 block number and block hash with the given block number from a RPC call
+func (cc *OPStackL2ConsumerController) QueryBlock(height uint64) (*types.BlockInfo, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), cc.cfg.Timeout)
+	defer cancel()
 
+	l2Block, err := cc.opl2Client.BlockByNumber(ctx, new(big.Int).SetUint64(height))
+	if err != nil {
+		return nil, err
+	}
 	return &types.BlockInfo{
 		Height: height,
-		Hash:   nil,
+		Hash:   l2Block.Hash().Bytes(),
 	}, nil
 }
 
-func (ec *OPStackL2ConsumerController) QueryIsBlockFinalized(height uint64) (bool, error) {
-	/* TODO: implement
-	1. get the latest finalized block number from `latestBlockNumber()` in the L1 L2OutputOracle contract
-	2. compare the block number with `height`
-	*/
+func (cc *OPStackL2ConsumerController) QueryIsBlockFinalized(height uint64) (bool, error) {
 	return false, nil
 }
 
-func (ec *OPStackL2ConsumerController) QueryActivatedHeight() (uint64, error) {
-	/* TODO: implement
-
-		oracle_event = query the event in the L1 oracle contract where the FP's voting power is firstly set
-
-		l1_activated_height = get the L1 block number from the `oracle_event`
-
-	  output_event = query the L1 event `emit OutputProposed(_outputRoot, nextOutputIndex(), _l2BlockNumber, block.timestamp, block.number);`
-				to find the first event where the `block.number` >= l1_activated_height
-
-		if output_event == nil:
-
-				read `nextBlockNumber()` from the L1 L2OutputOracle contract and return the result
-
-		else:
-
-				return output_event._l2BlockNumber
-
-	*/
-
+func (cc *OPStackL2ConsumerController) QueryActivatedHeight() (uint64, error) {
 	return 0, nil
 }
 
-func (ec *OPStackL2ConsumerController) QueryLatestBlockHeight() (uint64, error) {
-	/* TODO: implement
-	get the latest L2 block number from a RPC call
-	*/
+// QueryLatestBlockHeight gets the latest L2 block number from a RPC call
+func (cc *OPStackL2ConsumerController) QueryLatestBlockHeight() (uint64, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), cc.cfg.Timeout)
+	defer cancel()
 
-	return uint64(0), nil
+	return cc.opl2Client.BlockNumber(ctx)
 }
 
 // QueryLastCommittedPublicRand returns the last public randomness commitments
 func (ec *OPStackL2ConsumerController) QueryLastCommittedPublicRand(fpPk *btcec.PublicKey, count uint64) (map[uint64]*finalitytypes.PubRandCommitResponse, error) {
-
 	return nil, nil
 }
 
-func (ec *OPStackL2ConsumerController) Close() error {
-	return ec.bbnClient.Stop()
+func (cc *OPStackL2ConsumerController) Close() error {
+	cc.opl2Client.Close()
+	return cc.bbnClient.Stop()
 }
