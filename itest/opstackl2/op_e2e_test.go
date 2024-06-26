03350b2f8cd06b5e1960aad23062bd726e1afd8b
bap2pecs <bap2pecs@proton.me>
2024-06-26 11:41:49 -0400

    comments

--- itest/opstackl2/op_e2e_test.go
+++ itest/opstackl2/op_e2e_test.go
@@ -187,8 +187,25 @@ func TestBlockBabylonFinalized(t *testing.T) {
 	}
 
 	queryParams := &sdk.L2Block{
-		BlockHeight:    lastCommittedStartHeight,
-		BlockHash:      hex.EncodeToString(mockHash),
+		BlockHeight: lastCommittedStartHeight,
+		BlockHash:   hex.EncodeToString(mockHash),
+		/*
+			this is BTC height 10's timestamp: https://mempool.space/block/10
+
+			we use it b/c in InsertBTCDelegation(), it inserts at BTC block 0 because
+			- `tm.BBNClient.QueryBtcLightClientTip()` returns block 0
+			- `params.ComfirmationTimeBlocks` defaults to be 6, meaning the delegation
+			becomes active around block 6
+
+			Note: the staking time is default defined in utils.go to be 100 blocks:
+			StakingTime           = uint16(100)
+
+			since we only mock the first few BTC blocks and inject into the local
+			babylon chain, the delegation will be active forever. So even if we choose
+			a very large real BTC mainnet's block timestamp, the test will still pass.
+
+			But to be safe, we decided to choose the timestamp of block 10.
+		*/
 		BlockTimestamp: uint64(1231473952),
 	}
 	finalized, err := ctm.SdkClient.QueryIsBlockBabylonFinalized(queryParams)
