67c22fe18ae174a1aedd81c8c26bb33848c22cbb
bap2pecs <bap2pecs@proton.me>
2024-06-27 11:21:41 -0400

    use Validate()

--- clientcontroller/babylon/consumer.go
+++ clientcontroller/babylon/consumer.go
@@ -315,6 +315,10 @@ func (bc *BabylonConsumerController) QueryLastPublicRandCommit(fpPk *btcec.Publi
 		}
 	}
 
+	if err := commit.Validate(); err != nil {
+		return nil, err
+	}
+
 	return commit, nil
 }
 
