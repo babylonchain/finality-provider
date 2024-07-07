9f50351eb5c4eb9b48368a50e86fc3bbe9a58dc0
bap2pecs <bap2pecs@proton.me>
2024-07-07 14:14:56 -0400

    nit

--- testutil/log/log.go
+++ testutil/log/log.go
@@ -5,6 +5,7 @@ import (
 	"time"
 )
 
+// wraps around testing.T.Logf to add a timestamp prefix
 func Logf(t *testing.T, format string, args ...any) {
 	currentTime := time.Now().Format("[15:04:05.000]")
 	prefixedFormat := currentTime + " " + format
