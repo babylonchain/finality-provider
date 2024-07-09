56a98e2a425f3fcd88b26a32f85709ddd487aea1
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-07-09 11:02:19 -0300

    chore: remove unnecessary flags

--- finality-provider/cmd/fpcli/daemon/flags.go
+++ finality-provider/cmd/fpcli/daemon/flags.go
@@ -3,16 +3,11 @@ package daemon
 const (
 	fpdDaemonAddressFlag = "daemon-address"
 	keyNameFlag          = "key-name"
-	homeFlag             = "home"
-	fpBTCPkFlag          = "btc-pk"
-	blockHeightFlag      = "height"
 	appHashFlag          = "app-hash"
 	passphraseFlag       = "passphrase"
 	hdPathFlag           = "hd-path"
 	chainIdFlag          = "chain-id"
 	signedFlag           = "signed"
-	defaultPassphrase    = ""
-	defaultHdPath        = ""
 
 	// flags for description
 	monikerFlag         = "moniker"
