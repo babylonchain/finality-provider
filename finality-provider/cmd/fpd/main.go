e5969c5021871acce3e6d95f7be222e404ab1cf3
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-07-09 18:32:52 -0300

    fix: keys list parse with legacy amino from bbn

--- finality-provider/cmd/fpd/main.go
+++ finality-provider/cmd/fpd/main.go
@@ -4,11 +4,11 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/babylonchain/babylon/app"
 	"github.com/cosmos/cosmos-sdk/client"
 	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
 
-	appparams "github.com/babylonchain/babylon/app/params"
 	"github.com/babylonchain/finality-provider/finality-provider/cmd/fpd/daemon"
 	fpcfg "github.com/babylonchain/finality-provider/finality-provider/config"
 )
@@ -18,7 +18,6 @@ func NewRootCmd() *cobra.Command {
 	var (
 		clientCtx client.Context
 	)
-	encCfg := appparams.DefaultEncodingConfig()
 
 	rootCmd := &cobra.Command{
 		Use:           "fpd",
@@ -26,12 +25,24 @@ func NewRootCmd() *cobra.Command {
 		Long:          `fpd is the daemon to create and manage finality providers.`,
 		SilenceErrors: false,
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
+			// TODO(verify): if it uses the default encoding config it fails to list keys! output:
+			// "xx" is not a valid name or address: unable to unmarshal item.Data:
+			// Bytes left over in UnmarshalBinaryLengthPrefixed, should read 10 more bytes but have 154
+			// [cosmos/cosmos-sdk@v0.50.6/crypto/keyring/keyring.go:973
+			tempApp := app.NewTmpBabylonApp()
+
+			clientCtx = clientCtx.
+				WithCodec(tempApp.AppCodec()).
+				WithInterfaceRegistry(tempApp.InterfaceRegistry()).
+				WithTxConfig(tempApp.TxConfig()).
+				WithLegacyAmino(tempApp.LegacyAmino()).
+				WithInput(os.Stdin)
+
 			// set the default command outputs
 			cmd.SetOut(cmd.OutOrStdout())
 			cmd.SetErr(cmd.ErrOrStderr())
 
 			clientCtx = clientCtx.WithCmdContext(cmd.Context())
-			clientCtx.Codec = encCfg.Codec
 			if err := client.SetCmdClientContextHandler(clientCtx, cmd); err != nil {
 				return err
 			}
