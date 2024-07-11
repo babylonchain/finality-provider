c338d0b7653eb7666d8f143f53c1234bd3a5b783
RafilxTenfen <rafaeltenfen.rt@gmail.com>
2024-07-11 15:42:05 -0300

    chore: add load of defaults values from bbn config in context

--- finality-provider/cmd/cmd.go
+++ finality-provider/cmd/cmd.go
@@ -3,13 +3,17 @@ package cmd
 import (
 	"os"
 
-	"github.com/babylonchain/babylon/app"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/client/flags"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
+
+	"github.com/babylonchain/babylon/app"
+	fpcfg "github.com/babylonchain/finality-provider/finality-provider/config"
 )
 
 // PersistClientCtx persist some vars from the cmd to the client context.
-func PersistClientCtx(clientCtx client.Context) func(cmd *cobra.Command, _ []string) error {
+func PersistClientCtx(ctx client.Context) func(cmd *cobra.Command, _ []string) error {
 	return func(cmd *cobra.Command, _ []string) error {
 		// TODO(verify): if it uses the default encoding config it fails to list keys! output:
 		// "xx" is not a valid name or address: unable to unmarshal item.Data:
@@ -17,7 +21,7 @@ func PersistClientCtx(clientCtx client.Context) func(cmd *cobra.Command, _ []str
 		// [cosmos/cosmos-sdk@v0.50.6/crypto/keyring/keyring.go:973
 		tempApp := app.NewTmpBabylonApp()
 
-		clientCtx = clientCtx.
+		ctx = ctx.
 			WithCodec(tempApp.AppCodec()).
 			WithInterfaceRegistry(tempApp.InterfaceRegistry()).
 			WithTxConfig(tempApp.TxConfig()).
@@ -28,13 +32,58 @@ func PersistClientCtx(clientCtx client.Context) func(cmd *cobra.Command, _ []str
 		cmd.SetOut(cmd.OutOrStdout())
 		cmd.SetErr(cmd.ErrOrStderr())
 
-		clientCtx = clientCtx.WithCmdContext(cmd.Context())
-		if err := client.SetCmdClientContextHandler(clientCtx, cmd); err != nil {
+		ctx = ctx.WithCmdContext(cmd.Context())
+		if err := client.SetCmdClientContextHandler(ctx, cmd); err != nil {
 			return err
 		}
 
-		return nil
+		ctx = client.GetClientContextFromCmd(cmd)
+		// check the config file exists
+		cfg, err := fpcfg.LoadConfig(ctx.HomeDir)
+		if err != nil {
+			return nil // if no conifg is found just stop.
+		}
+
+		// config was found, load the defaults if not set by flag
+		// flags have preference over config.
+		ctx, err = FillContextFromBabylonConfig(ctx, cmd.Flags(), cfg.BabylonConfig)
+		if err != nil {
+			return err
+		}
+
+		// updates the ctx in the cmd in case something was modified bt the config
+		return client.SetCmdClientContext(cmd, ctx)
 	}
+}
+
+// FillContextFromBabylonConfig loads the bbn config to the context if values were not set by flag.
+// Preference is FlagSet values over the config.
+func FillContextFromBabylonConfig(ctx client.Context, flagSet *pflag.FlagSet, bbnConf *fpcfg.BBNConfig) (client.Context, error) {
+	if !flagSet.Changed(flags.FlagFrom) {
+		ctx = ctx.WithFrom(bbnConf.Key)
+	}
+	if !flagSet.Changed(flags.FlagChainID) {
+		ctx = ctx.WithChainID(bbnConf.ChainID)
+	}
+	if !flagSet.Changed(flags.FlagKeyringBackend) {
+		kr, err := client.NewKeyringFromBackend(ctx, bbnConf.KeyringBackend)
+		if err != nil {
+			return ctx, err
+		}
+
+		ctx = ctx.WithKeyring(kr)
+	}
+	if !flagSet.Changed(flags.FlagKeyringDir) {
+		ctx = ctx.WithKeyringDir(bbnConf.KeyDirectory)
+	}
+	if !flagSet.Changed(flags.FlagOutput) {
+		ctx = ctx.WithOutputFormat(bbnConf.OutputFormat)
+	}
+	if !flagSet.Changed(flags.FlagSignMode) {
+		ctx = ctx.WithSignModeStr(bbnConf.SignModeStr)
+	}
+
+	return ctx, nil
 }
 
 // RunEWithClientCtx runs cmd with client context and returns an error.
